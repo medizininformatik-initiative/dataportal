101dcce7ca7d0943ccc3fed73523a7a6270265e7
Mathias Rühle <math2306@hotmail.com>
2026-04-10 13:41:12 +0200

    Downloading IGS independently of working directory

--- data-node/fhir-validator/get-igs.sh
+++ data-node/fhir-validator/get-igs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -L https://packages.fhir.org/de.medizininformatikinitiative.kerndatensatz.base/2026.0.0 -o igs/de.medizininformatikinitiative.kerndatensatz.base-2026.0.0.tgz
+BASE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P )"
+
+curl -L https://packages.fhir.org/de.medizininformatikinitiative.kerndatensatz.base/2026.0.0 -o "$BASE_DIR/igs/de.medizininformatikinitiative.kerndatensatz.base-2026.0.0.tgz"
